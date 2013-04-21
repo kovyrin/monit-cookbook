a1f1ab5fbec213255adf8554e6ec1e0024e75476
Scott Bader <sb@scottbader.org>
2013-04-21 14:31:42 -0400

    monit_watch lwrp

---
+++ providers/watch.rb
@@ -0,0 +1,46 @@
+#
+# Cookbook Name:: Monit
+# Provider:: watch
+#
+# Copyright 2011, gotryiton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+action :enable do
+  pid_file = new_resouce.pid_file || "/var/run/#{new_resource.name}.pid"
+  start_command = new_resouce.start_command || "/etc/init.d/#{new_resource.name} start"
+  stop_command = new_resouce.stop_command || "/etc/init.d/#{new_resource.name} stop"
+
+  template "/etc/monit/conf.d/#{new_resource.name}.conf" do
+    owner "root"
+    group "root"
+    mode "644"
+    variables name: new_resource.name,
+      pid_file: pid_file,
+      start_command: start_command,
+      stop_command: stop_command,
+      restart_timeout_count: new_resource.restart_timeout_count,
+      cycle_timeout_count: new_resource.cycle_timeout_count
+
+    source 'monit_resource.conf.erb'
+    notifies :restart, "service[monit]"
+  end
+end
+
+action :disable do
+  template "/etc/monit/conf.d/#{new_resource.name}.conf" do
+    action :delete
+    notifies :restart, "service[monit]"
+  end
+end
---
+++ resources/watch.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: Monit
+# Resource:: watch
+#
+# Copyright 2011, gotryiton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+actions :enable, :disable
+
+default_action :enable
+
+attribute :name, :kind_of => String, :name_attribute => true
+attribute :pid_file, :kind_of => String
+attribute :start_command, :kind_of => String, :required => true
+attribute :stop_command, :kind_of => String, :required => true
+attribute :restart_timeout_count, :kind_of => Integer, :default => 5
+attribute :cycle_timeout_count, :kind_of => Integer, :default => 5
